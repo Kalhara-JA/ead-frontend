cf7bee8a1d00209b6f478d6d1f6481198a0b6b4c
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-11-18 15:27:41 +0530

    product API updated

--- app/api/v1/product/route.tsx
+++ app/api/v1/product/route.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchProducts = async () => {
   try {
-    const response = await axios.get( `${process.env.NEXT_PUBLIC_API_URL}/product` );
+    const response = await axios.get( `${process.env.NEXT_PUBLIC_API_URL}/products` );
     console.log(response);
     return response.data; // Axios stores the response data in `data`
   } catch (error) {
