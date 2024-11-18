232109f2c2bed41ac7f7a932f5e82729cb743bcd
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-11-18 11:06:25 +0530

    .env added

--- app/api/v1/product/route.tsx
+++ app/api/v1/product/route.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchProducts = async () => {
   try {
-    const response = await axios.get("http://localhost:8080/api/v1/product");
+    const response = await axios.get( `${process.env.REACT_APP_API_URL}/products` );
     console.log(response);
     return response.data; // Axios stores the response data in `data`
   } catch (error) {
